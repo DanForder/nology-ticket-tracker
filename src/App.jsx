69fe09101ac5e33238e97ba3851c6e1b218e70d3
Dan Forder <danfrdr@gmail.com>
2021-11-30 14:55:06 +0000

    Added ticket component, styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Ticket from "./components/Ticket/Ticket";
+import "./styles/main.scss";
+
 const App = () => {
-  return <h1>Hello There</h1>;
+  return (
+    <>
+      <h1>Ticket Tracker</h1>
+      <Ticket />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Ticket/Ticket.jsx
@@ -0,0 +1,13 @@
+import "./_Ticket.scss";
+
+const Ticket = () => {
+  return (
+    <div className="ticket">
+      <span className="ticket__name"></span>
+      <span className="ticket__role"></span>
+      {/* <Counter/> */}
+    </div>
+  );
+};
+
+export default Ticket;
