f4da54dd88ddfa5abca286b3e2447cf81a194ee6
Dan Forder <danfrdr@gmail.com>
2021-12-14 22:18:25 +0000

    split files up

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+ReactDOM.render
+(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
